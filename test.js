5750c02a015674ef0578876357cf8d3f8ba14448
marcos <laina855@gmail.com>
2023-11-03 17:15:49 +0100

    deleted test

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
 const resolved = require('./index.js')
 
-test('Mensajes encriptados - llave casa', () => {
-  expect(resolved('llaveS casa CASA casa llaves')).toBe('llaves2casa3')
-})
-
-test('Mensajes encriptados - taza ta za', () => {
-  expect(resolved('taza ta za taza')).toBe('taza2ta1za1')
-})
-
-test('Mensajes encriptados - casas casa casasas', () => {
-  expect(resolved('casas casa casasas')).toBe('casas1casa1casasas1') // en el enunciado era casas1casa1casas1 :( ?
+test('test', () => {
+  expect(resolved()).toBe('')
 })
